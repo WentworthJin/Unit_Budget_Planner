be700aad714274b414f8b6298550174e4f57bfae
lida <22957193@student.uwa.edu.au>
2021-09-09 17:05:55 +0800

    update UI

---
+++ Unit Budget/src/index.js
@@ -0,0 +1,9 @@
+// This method is for role identification
+function myFunction() {
+    var x = document.getElementById("role");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
