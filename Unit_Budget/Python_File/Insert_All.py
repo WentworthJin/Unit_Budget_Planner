6bb4a93cf7cea0182cc5ea3a41626e038a1a07b2
Ethan-Chen <23067035@student.uwa.edu.au>
2021-09-21 02:02:27 +0800

    Update Insert_All.py
    done every table except activities. still working on it. try to push all other stuff for now

--- Unit_Budget/Python_File/Insert_All.py
+++ Unit_Budget/Python_File/Insert_All.py
@@ -1,6 +1,7 @@
 import sqlite3
 import sys
 import pandas as pd
+import numbers
 from sqlite3 import Error
 
 TeachingCode = ["TeachingName"]
@@ -94,19 +95,83 @@ def insert_budget(conn, budget):
       conn.commit()
       return cur.lastrowid
 
+
+#Insert data into OtherCost table
+def insert_otherCost(conn, otherCost):
+    sql = ''' Insert into OtherCost(NSCID,UnitID)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    data_check=cur.execute(sql, otherCost)
+    # Check if data already exist
+    if data_check is None:
+      cur.execute(sql, otherCost)
+      conn.commit()
+      return cur.lastrowid
+
+
+
+#Insert data into Staff table
+def insert_staff(conn, staff):
+    sql = ''' Insert into Staff(TeachingID,Name,Postion)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    data_check=cur.execute(sql, staff)
+    # Check if data already exist
+    if data_check is None:
+      cur.execute(sql, staff)
+      conn.commit()
+      return cur.lastrowid
+
+# retrieve information from excel file to dataframe 
 def get_details(file):
-  unit_detail = pd.read_excel(file,usecols ="A:B",header =8,nrows=5)
+  unit_detail = pd.read_excel(file,usecols ="A:L",header =8,nrows=5)
   unit_strcture = pd.read_excel(file,usecols ="A:J",header =29,nrows=13)
   resourcing = pd.read_excel(file,usecols ="A:L",header =46,nrows=9)
   NSC = pd.read_excel(file,usecols ="A:F",header =62,nrows=4)
   return unit_detail,unit_strcture,resourcing,NSC
 
-
+# check if a session is makring session or not 
 def check_session(SessionName):
   if 'mark' in SessionName.lower():
     return 'M'
   else:
     return 'NM'
+
+# check if this value is numberic
+def check_numbers(number):
+  return isinstance(number, numbers.Number)
+
+
+
+def select_StaffID(conn,Name):
+
+  QueryTemplate = '''Select StaffID From Staff Where Name = {0};'''
+
+  query = QueryTemplate.format(Name)
+
+  cur = conn.cursor()
+  cur.execute(query)
+
+  rows = cur.fetchall()
+
+  return rows[0][0]
+
+
+def select_TeachingCode(conn,TeachingName):
+
+  QueryTemplate = '''Select TeachingCode From TeachingCode Where TeachingName = {0};'''
+
+  query = QueryTemplate.format(TeachingName)
+
+  cur = conn.cursor()
+  cur.execute(query)
+
+  rows = cur.fetchall()
+
+  return rows[0][0]
+
+#s = select_StaffID(conn,'''"Racheal"''') 
+
 
 
 
@@ -115,6 +180,11 @@ def main():
   pd.set_option("max_columns", 10)
   excel_file ="CITS1001_Sem1,2021 budget v3.xlsx"
   unit_detail,unit_strcture,resourcing,NSC = get_details(excel_file)
+
+  thisyear_detail = unit_detail.iloc[:,3:7]
+
+  lastyear_detail = unit_detail.iloc[:,8:12]
+
 
   UnitCode =unit_detail.iloc[0,1]
   Semester=unit_detail.iloc[1,1]
@@ -139,12 +209,22 @@ def main():
 
       #Insert Unit table
       Unit = (UnitCode,Semester,Year)
-      insert_unit(conn,Unit)
+      UnitID = insert_unit(conn,Unit)
 
       #Insert TeachingCode table
       for TeachingName in teachingcodes:
         TeachingCode = [TeachingName]
         insert_teachingcode(conn,TeachingCode)
+
+
+      #Insert Staff table
+      for i in range(len(teachingcodes)):
+        teachingID = select_StaffID(conn,resourcing.iloc[0:7,3][i])
+        teacherName = resourcing.iloc[0:7,0][i]
+        teacherPostion = resourcing.iloc[0:7,1][i]
+        staff = (teachingID,teacherName,teacherPostion)
+        insert_session(conn,staff)
+
 
       #Insert Session table
       for SessionName in sessionNames:
@@ -153,22 +233,50 @@ def main():
         insert_session(conn,session)
 
 
-      #Insert NonSalaryCost table
+      #Insert NonSalaryCost table & OtherCost Table
       for i in range(len(NSCCost)):
         NSCName = NSCNames[i]
         Hours = NSCHours[i]
         CostPerHour = NSCRates[i]
         TotalCost = int(NSCCost[i])
         nsc = (NSCName,Hours,CostPerHour,TotalCost)
-        insert_nsc(conn,nsc)
+        NSCID = insert_nsc(conn,nsc)
 
-      #Insert Enrolment table
-      #enrol = (UnitID,EnrolmentNumber,IsEstimated,IsLastSemester)
-      #insert_enrolment(conn,enrol)
+        otherCost = (NSCID,UnitID)
+        insert_session(conn,otherCost)
 
-      #Insert Budget table
-      #budget = (UnitID,Cost,IsEstimated,IsLastSemester)
-      #insert_budget(conn,budget)
+      #Insert Enrolment table & Budget Table
+      for i in range(len(thisyear_detail.index)):
+        if 'estimated enrolment' in thisyear_detail.iloc[:,0][i].lower():
+          if check_numbers(thisyear_detail.iloc[:,3][i]):
+            enrol = (UnitID,thisyear_detail.iloc[:,3][i],'Yes','No')
+            insert_enrolment(conn,enrol)
+        if 'budget' in thisyear_detail.iloc[:,0][i].lower():
+          if check_numbers(thisyear_detail.iloc[:,3][i]):
+            budget = (UnitID,thisyear_detail.iloc[:,3][i],'Yes','No')
+            insert_budget(conn,budget)
+
+      for i in range(len(lastyear_detail.index)):
+        if 'estimated enrolment' in lastyear_detail.iloc[:,0][i].lower():
+          if check_numbers(lastyear_detail.iloc[:,3][i]):
+            enrol = (UnitID,lastyear_detail.iloc[:,3][i],'Yes','Yes')
+            insert_enrolment(conn,enrol)
+
+        if 'actual enrolment' in lastyear_detail.iloc[:,0][i].lower():
+          if check_numbers(lastyear_detail.iloc[:,3][i]):
+            enrol = (UnitID,lastyear_detail.iloc[:,3][i],'No','Yes')
+            insert_enrolment(conn,enrol)
+
+        if 'budget' in lastyear_detail.iloc[:,0][i].lower():
+          if check_numbers(lastyear_detail.iloc[:,3][i]):
+            budget = (UnitID,lastyear_detail.iloc[:,3][i],'Yes','Yes')
+            insert_enrolment(conn,enrol)
+
+        if 'actual expense' in lastyear_detail.iloc[:,0][i].lower():
+          if check_numbers(lastyear_detail.iloc[:,3][i]):
+            budget = (UnitID,lastyear_detail.iloc[:,3][i],'No','Yes')
+            insert_enrolment(conn,enrol)
+
 
     print("Dummy Unit data has been inserted")
 
