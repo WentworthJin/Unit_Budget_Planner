f55207a9bd5351fa98a0fbc3f49c89bf6575308d
lida <22957193@student.uwa.edu.au>
2021-09-24 11:21:16 +0800

    delete testing

--- Unit_Budget/Unit testing file/Unit testing_Lida.py
+++
@@ -1,6 +0,0 @@
-import wget
-
-print('Beginning file download with wget module')
-
-url = 'https://github.com/WentworthJin/Unit_Budget_Planner/raw/master/Unit_Budget/Unit%20testing%20file/CITS4401_Sem1%202021%20budgetv3.xlsx'
-wget.download(url, '../Downloads/CITS4401_Sem1 2021 budgetv3')
