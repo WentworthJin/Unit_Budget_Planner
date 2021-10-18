a25f9120474dc473ad6172f014556dea4d62d235
Nara <Sameam90@gmail.com>
2021-10-18 12:42:48 +0800

    solve some conflict

--- Unit_Budget/Python_File/route.py
+++ Unit_Budget/Python_File/route.py
@@ -4,11 +4,13 @@ from Create_Table import Schema
 from Insert_All_Liangbo_Version import sample_insert
 import sqlite3 
 import os.path
+import sys
 
 # Initilize the Database
 Schema()
 
 # Insert mock data
+# sample_insert()
 
 # File Type Limit
 ALLOWED_EXTENSIONS = {'xlsx'}
@@ -17,7 +19,17 @@ ALLOWED_EXTENSIONS = {'xlsx'}
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, './uploads')
 
-app = Flask(__name__,template_folder='../dist',static_folder='../src')
+def resource_path(relative_path):
+  base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.dirname(__file__)))
+  return os.path.join(base_path, relative_path) 
+
+
+if hasattr(sys, '_MEIPASS'):
+  template_folder =resource_path('dist')
+  static_folder = resource_path('src')
+  app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else: 
+  app = Flask(__name__,template_folder=resource_path('dist'),static_folder=resource_path('src'))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # get the absolute path for the current directory
@@ -28,6 +40,34 @@ db_path = os.path.join(BASE_DIR, "BudgetSample (1).db")
 @app.route("/", methods=["GET"])
 def render():
   return render_template("index.html")
+
+def buildWhereClause(data): 
+  data = request.args.to_dict()
+  params = data.keys()
+  s1 = 'U.UnitCode ="{}" '.format(data['unitcode']) if 'unitcode' in params else ''
+  s2 = 'U.Year = ' + data['year'] if 'year' in params else ''
+  s3 = 'U.Semester = ' + data['semester'] if 'semester' in params else ''
+  s4 = 'substr(U.UnitCode, 5, 1) ="{}" '.format(data['unitLevel']) if 'unitLevel' in params else '' 
+  s = list()
+  for x in [s1, s2, s3, s4]:
+      if x:
+          s.append(x)
+  queryStrings = ' and '.join(s) 
+  return queryStrings
+
+def buildJoinClause(data): 
+  data = request.args.to_dict()
+  params = data.keys()
+  s1 = 'U.UnitCode ="{}" '.format(data['unitcode']) if 'unitcode' in params else ''
+  s2 = 'U.Year = ' + data['year'] if 'year' in params else ''
+  s3 = 'U.Semester = ' + data['semester'] if 'semester' in params else ''
+  s = list()
+  for x in [s1, s2, s3]:
+      if x:
+          s.append(x)
+  requestStrings = ' and '.join(s) 
+  return requestStrings
+
 
 
 @app.route("/get", methods=["GET"])
@@ -44,7 +84,7 @@ def get_all_data():
           JOIN Unit R USING (UnitID) \
           Where R.UnitCode = U.UnitCode \
           ) AS Num_of_Staff, \
-          SUM(A.Hour * A.HourlyRate) AS StaffCost, \
+          SUM(A.Hour * A.PayRate) AS StaffCost, \
           (Select SUM(N.TotalCost) \
           From OtherCost O JOIN NonSalaryCosts N USING (NSCID) \
           JOIN UNIT Z USING (UnitID) \
@@ -65,7 +105,14 @@ def get_all_data():
           (Select SUM(A.Hour) \
           From Activities A JOIN Unit N USING (UnitID) \
           Where N.UnitID = U.UnitID \
-          Group by N.UnitID) AS Total_WorkLoad \
+          Group by N.UnitID) AS Total_WorkLoad, \
+          (Select En.EnrolmentNumber \
+          FROM Enrolment En \
+          JOIN Budget B USING (UnitID) \
+          Where En.IsEstimated = "YES" and En.IsLastSemester = "NO" and En.UnitID = U.UnitID ) AS Enrolment_number, \
+          (Select B.cost / En.EnrolmentNumber from Budget B JOIN Enrolment En USING (UnitID) \
+          Where B.IsEstimated="YES" and B.IsLastSemester="NO" and En.IsEstimated="YES" \
+          and En.IsLastSemester="NO" and En.UnitID = U.UnitID) AS Cost_per_student \
           From Activities A JOIN Staff S USING (StaffID)  \
                                         JOIN Session E USING (SessionID) \
                                         JOIN Unit U USING (UnitID) \
@@ -87,20 +134,147 @@ def get_all_data():
 
 @app.route("/get_all_data", methods=["GET"])
 def get_main_data():
-  
   """The function is used to get the  main data from database and send to client side 
-
    Parameters: There is no parameter needed for this one. 
    """
+  queryStrings = buildWhereClause(request.args.to_dict())
   con = sqlite3.connect(db_path)
   cur = con.cursor()
-  cur.execute('Select U.UnitCode, SUM(A.Hour) AS TotalLoad, U.Semester,U.Year, \
+  sql = 'Select U.UnitCode, SUM(A.Hour) AS TotalLoad, U.Semester,U.Year, \
                       (Select COUNT(DISTINCT P.Name) \
                       From Activities A JOIN Staff P USING (StaffID) \
                       JOIN Unit R USING (UnitID) \
                       Where R.UnitCode = U.UnitCode \
                       ) AS Num_of_Staff, \
-                      SUM(A.Hour * A.HourlyRate) AS StaffCost, \
+                      SUM(A.Hour * A.PayRate) AS StaffCost, \
+                      (Select SUM(N.TotalCost) \
+                      From OtherCost O JOIN NonSalaryCosts N USING (NSCID) \
+                      JOIN UNIT Z USING (UnitID) \
+                      Where Z.UnitID = U.UnitID \
+                      Group by Z.UnitID) AS NonSalaryCost, \
+                      (Select B.Cost \
+                      From Unit G JOIN Budget B USING (UnitID) \
+                      Where IsEstimated = "YES" and B.IsLastSemester = "NO" and G.UnitID = U.UnitID ) AS Budget, \
+                      (Select COUNT(*) \
+                      From Activities A JOIN Unit P USING (UnitID) \
+                      Where P.UnitID = U.UnitID \
+                      Group by P.UnitID) AS TotalActivities, \
+                      (Select COUNT(*) \
+                      From OtherCost O JOIN Unit L USING (UnitID) \
+                      JOIN NonSalaryCosts USING (NSCID) \
+                      Where L.UnitID = U.UnitID \
+                      Group by L.UnitID) AS Total_Number_of_NSC, \
+                      (Select SUM(A.Hour) \
+                      From Activities A JOIN Unit N USING (UnitID) \
+                      Where N.UnitID = U.UnitID \
+                      Group by N.UnitID) AS Total_WorkLoad, \
+                      (Select En.EnrolmentNumber \
+                      FROM Enrolment En \
+                      JOIN Budget B USING (UnitID) \
+                      Where En.IsEstimated = "YES" and En.IsLastSemester = "NO" and En.UnitID = U.UnitID ) AS Enrolment_number, \
+                      (Select B.cost / En.EnrolmentNumber from Budget B JOIN Enrolment En USING (UnitID) \
+                      Where B.IsEstimated="YES" and B.IsLastSemester="NO" and En.IsEstimated="YES" \
+                      and En.IsLastSemester="NO" and En.UnitID = U.UnitID) AS Cost_per_student \
+                      From Activities A JOIN Staff S USING (StaffID)  \
+                                                    JOIN Session E USING (SessionID) \
+                                                    JOIN Unit U USING (UnitID) \
+                      '
+  if queryStrings:
+    sql = sql + ''' where ''' + queryStrings    
+  cur.execute(sql + " Group by U.UnitID ") 
+  result = cur.fetchall()
+  con.close()
+  return jsonify(result)
+
+
+# route to get each employees in each unit budget
+@app.route("/employee_budget", methods=["GET"])
+def get_employee_budget():
+  """The function is used to get the employees budget data from database and send to client side 
+
+   Parameters: There is no parameter needed for this one. 
+   """
+  queryStrings = buildWhereClause(request.args.to_dict())
+  con = sqlite3.connect(db_path)
+  cur = con.cursor()
+  sql = "Select S.Name, U.UnitCode, U.Semester, U.Year, SUM(A.Hour*A.PayRate) AS TotalCost \
+        From Activities A JOIN Staff S USING (StaffID) \
+        JOIN Session E USING (SessionID) \
+        JOIN Unit U USING (UnitID) "
+  if queryStrings:
+    sql = sql + ''' where ''' + queryStrings         
+  cur.execute(sql + " Group by S.StaffID ") 
+  result = cur.fetchall()
+  con.close()
+  return jsonify(result)
+
+# get workload and total cost for each unit 
+@app.route("/workload", methods=["GET"])
+def get_semester_budget():
+  """The function is used to get the workload and budget data from database and send to client side 
+
+   Parameters: There is no parameter needed for this one. 
+   """
+  queryStrings = buildWhereClause(request.args.to_dict())
+  con = sqlite3.connect(db_path)
+  cur = con.cursor()
+  sql = "Select U.UnitCode, SUM(A.Hour) AS TotalLoad, SUM(A.Hour * A.PayRate) AS StaffCost \
+              From Activities A JOIN Staff S USING (StaffID) \
+                JOIN Session E USING (SessionID) \
+                JOIN Unit U USING (UnitID) \
+              "
+  if queryStrings:
+    sql = sql + ''' where ''' + queryStrings
+  sql = sql + " Group By U.UnitID "
+  cur.execute(sql)
+  result = cur.fetchall()
+  con.close()
+  return jsonify(result)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+@app.route("/comment", methods=["GET"])
+def get_comment():
+  requestStrings = buildJoinClause(request.args.to_dict())
+  con = sqlite3.connect(db_path)
+  cur = con.cursor()
+  sql = "Select U.UnitCode, A.Comment \
+        from Activities A \
+        JOIN Unit U USING (UnitID)"
+  if requestStrings:
+    sql = sql + ''' where ''' + requestStrings 
+  sql = sql + " and A.Comment IS NOT NULL"
+  cur.execute(sql)
+  result = cur.fetchall()
+  con.close()
+  return jsonify(result)
+
+@app.route("/table",methods=['GET','POST'])
+def upload_file():
+  try:
+    if request.method == 'POST':
+      if 'filename' not in request.files:
+        return render()
+      file = request.files['filename']
+      if file.filename == '':
+            return render()
+      if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        Unit_ID = filename [0:8]
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    # Insert mock data
+    ID = Unit_ID
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    cur.execute('Select U.UnitCode, SUM(A.Hour) AS TotalLoad, U.Semester,U.Year, \
+                      (Select COUNT(DISTINCT P.Name) \
+                      From Activities A JOIN Staff P USING (StaffID) \
+                      JOIN Unit R USING (UnitID) \
+                      Where R.UnitCode = U.UnitCode \
+                      ) AS Num_of_Staff, \
+                      SUM(A.Hour * A.PayRate) AS StaffCost, \
                       (Select SUM(N.TotalCost) \
                       From OtherCost O JOIN NonSalaryCosts N USING (NSCID) \
                       JOIN UNIT Z USING (UnitID) \
@@ -126,67 +300,33 @@ def get_main_data():
                                                     JOIN Session E USING (SessionID) \
                                                     JOIN Unit U USING (UnitID) \
                       Group By U.UnitID')
-  result = cur.fetchall()
-  return jsonify(result)
-
-# route to get each employees in each unit budget
-@app.route("/employee_budget", methods=["GET"])
-def get_employee_budget():
-  
-  """The function is used to get the employees budget data from database and send to client side 
-
-   Parameters: There is no parameter needed for this one. 
-   """
-  con = sqlite3.connect(db_path)
-  cur = con.cursor()
-  cur.execute("Select S.Name, U.UnitCode, U.Semester, U.Year, SUM(A.Hour*A.HourlyRate) AS TotalCost \
-              From Activities A JOIN Staff S USING (StaffID) \
-                JOIN Session E USING (SessionID) \
-                JOIN Unit U USING (UnitID) \
-              Group by S.StaffID ") 
-  result = cur.fetchall()
-  return jsonify(result)
-
-# get workload and total cost for each unit 
-@app.route("/workload", methods=["GET"])
-def get_semester_budget():
-  
-  """The function is used to get the workload and budget data from database and send to client side 
-
-   Parameters: There is no parameter needed for this one. 
-   """
-  con = sqlite3.connect(db_path)
-  cur = con.cursor()
-  cur.execute("Select U.UnitCode, SUM(A.Hour) AS TotalLoad, SUM(A.Hour * A.HourlyRate) AS StaffCost \
-              From Activities A JOIN Staff S USING (StaffID) \
-                JOIN Session E USING (SessionID) \
-                JOIN Unit U USING (UnitID) \
-              Group By U.UnitID \
-              ")
-  result = cur.fetchall()
-  return jsonify(result)
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route("/table",methods=['GET','POST'])
-def upload_file():
-  try:
-    if request.method == 'POST':
-      if 'filename' not in request.files:
-        return render()
-      file = request.files['filename']
-      if file.filename == '':
-            return render()
-      if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    # Insert mock data
-    # sample_insert()
-    return render_template('table.html')
+    rows = cur.fetchall()
+    for row in rows:
+      if row[0] == ID:
+        data = row
+    return render_template('table.html',data = data)
   except:
     return render()
+
+@app.route('/customSqlQuery', methods=['POST'])
+def sqlquery():
+    try:
+        print(request.form.to_dict())
+        sql = request.form['sql']
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute(sql)
+        col_name_list = [tuple[0] for tuple in c.description]
+        content = c.fetchall()
+        conn.commit()
+        conn.close()
+        r = {'success':'true','data':list()}
+        for row in content:
+            d = dict(zip(col_name_list, row))
+            r['data'].append(d)
+        return jsonify(r)
+    except:
+        return {'success':'false'}    
 
 if __name__=="__main__":
   app.run(host='127.0.0.1', port=5000,debug=True)
--- Unit_Budget/src/index.js
+++ Unit_Budget/src/index.js
@@ -1,12 +1,25 @@
-// This method is for role identification
-  var el_down = document.getElementById("GFG_DOWN");
-  function GFG_click(clicked) {
-      el_down.innerHTML = "You are the "+clicked+".";
+// This method is for role identification and have different graphical Interface 
+var el_down = document.getElementById("GFG_DOWN");
+function GFG_click(clicked) {
+  el_down.innerHTML = "You are the "+clicked+".";
+  if (clicked=="Head of Department") {
+    document.getElementById('text').innerHTML="Please Upload an Excel Folder"
+    document.getElementById('text1').innerHTML="Please Choose the Folder"
+    document.getElementById('myFile1').style.display="inline-block"
+    document.getElementById('myFile').style.display="none"
   }
+  else {
+    document.getElementById('text').innerHTML="Please Upload Excel File"
+    document.getElementById('text1').innerHTML="Please Select the correct file format"
+    document.getElementById('myFile').style.display="inline-block"
+    document.getElementById('myFile1').style.display="none"
+  }
+}
 
 // document.getElementById('role1').onclick = function(){
 //     this.style.backgroundColor = 'Yellow';
 // };
+
 
 function showAndHide() {
   const x = document.getElementById("radio");
@@ -17,41 +30,82 @@ function showAndHide() {
   }
 }
 
-/**Used to get data from server side 
- * @param does not have parameter 
+/**
+ * Build SearchParams to include supplied queries.
+ * 
+ * @param year The year when a report is created.
+ * @param semester The semester a report targets to.
+ * @param unitcode The Unit code a report targets to.
+ * @param unitLevel Level of the unit.
+ */
+const buildSearchParams = (year, semester, unitcode, unitLevel) => {
+  const queryParams = new URLSearchParams();
+  if(year) {
+    queryParams.append("year", year);
+  }
+  if(semester) {
+    queryParams.append("semester", semester);
+  }
+  if(unitcode) {
+    queryParams.append("unitcode", unitcode);
+  }
+  if(unitLevel) {
+    queryParams.append("unitLevel", unitLevel);
+  }
+
+  return queryParams;
+}
+
+
+const buildCommentParams = (year, semester, unitcode) => {
+  const requestParams = new URLSearchParams();
+  if(year) {
+    requestParams.append("year", year);
+  }
+  if(semester) {
+    requestParams.append("semester", semester);
+  }
+  if(unitcode) {
+    requestParams.append("unitcode", unitcode);
+  }
+
+  return requestParams;
+}
+/**
  * function fetch the data and return a response of an array of data 
  * pass into the window.onload for plot the bar graph
-*/
-
-// get the data from database 
-const getAllData = () => {
-  fetch('http://127.0.0.1:5000/get_all_data', {
+ * 
+ * @param year The year when the report is created.
+ * @param semester The semester which the report targets to.
+ * @param unitcode The Unit code which the report targets to.
+ * @param unitLevel Level of the unit.
+ */ 
+const getAllData = async(year, semester, unitcode, unitLevel) => {
+  const queryParams = buildSearchParams(year, semester, unitcode, unitLevel);
+  const result = await fetch('http://127.0.0.1:5000/get_all_data?' + queryParams, {
     method:"GET",
     headers:{
       headers: {
         "Content-Type":"application/json"
       }
     }
-  })
-  .then(resp => resp.json())
-  .then((data) => 
-    window.onload(data))
-  .catch(error => 
-    console.log(error))
+  });
+
+  const data = await result.json();
+  window.onload(data);
+  return data;
 }
 
-/**Plot a bar graph 
+/**
+ * Plot a bar graph 
  * @param data type=array, receive data from server 
  * plot the data dynamically based on data from database 
-*/
-
-// drawing a bar chart 
-window.onload = function (data) {
+ */
+ window.onload = function (data) {
   const dataArray = []
   for(var i = 0; i < data.length; i++){
-    dataArray.push({"label":data[i][0], "y":data[i][7]})
+    dataArray.push({"label":data[i][0], "y":data[i][12]})
   } 
-  
   var chart = new CanvasJS.Chart("barchart", {
     animationEnabled: true,
     title:{
@@ -66,6 +120,7 @@ window.onload = function (data) {
      }
      ],
     axisY:{
+      minimum:0,
       prefix: "$",
     }     
     
@@ -74,13 +129,18 @@ window.onload = function (data) {
 }
 
 
-/**Used to get data from server side 
- * @param does not have parameter 
+/**
  * function fetch the data and return a response of an array of data 
  * pass into the horizontal bar chart for plot the horizontal bar chart
-*/
-const getEmployeeData = () => {
-  fetch('http://127.0.0.1:5000/employee_budget', {
+ * 
+ * @param year The year when the report is created.
+ * @param semester The semester which the report targets to.
+ * @param unitcode The Unit code which the report targets to.
+ * @param unitLevel Level of the unit.
+ */ 
+const getEmployeeData = (year, semester, unitcode, unitLevel) => {
+  const queryParams = buildSearchParams(year, semester, unitcode, unitLevel);
+  fetch('http://127.0.0.1:5000/employee_budget?' + queryParams, {
     method:"GET",
     headers:{
       headers: {
@@ -97,16 +157,16 @@ const getEmployeeData = () => {
 }
 
 
-/**Plot the horizontal bar graph 
+/**
+ * Plot the horizontal bar graph 
  * @param data, type=array, receive data from server 
  * plot the data dynamically based on data from database 
-*/
+ */
 const horizontalbarchart = function (data) {
   const dataArray = []
   for(var i = 0; i < data.length; i++){
-    dataArray.push({"y":data[i][4], "label":data[i][0]})
+    if (data[i][4]!==0) {dataArray.push({"y":data[i][4], "label":data[i][0]}) }
   };
-
 
   var horizontal = document.getElementById("horizontal")
   var bar_chart = new CanvasJS.Chart(horizontal, {
@@ -121,6 +181,7 @@ const horizontalbarchart = function (data) {
       }
     ],
     axisY:{
+      minimum:0,
       prefix: "$",
     }     
     
@@ -128,13 +189,18 @@ const horizontalbarchart = function (data) {
   bar_chart.render();
 }
 
-/**Used to get data from server side 
- * @param does not have parameter 
+/**
  * function fetch the data and return a response of an array of data 
- * pass into the graphing for plot the cluster bar chart
-*/
-const getWorkLoadData = function () {
-  fetch('http://127.0.0.1:5000/workload', {
+ * pass into the graphing for plot the cluster bar chart 
+ *
+ * @param year The year when the report is created.
+ * @param semester The semester which the report targets to.
+ * @param unitcode The Unit code which the report targets to.
+ * @param unitLevel Level of the unit.
+*/ 
+const getWorkLoadData = function (year, semester, unitcode, unitLevel) {
+  const queryParams = buildSearchParams(year, semester, unitcode, unitLevel);
+  fetch('http://127.0.0.1:5000/workload?' + queryParams, {
     method:"GET",
     headers:{
       headers: {
@@ -150,10 +216,11 @@ const getWorkLoadData = function () {
     console.log(error))
 }
 
-/**Plot the cluster bar graph 
+/**
+ * Plot the cluster bar graph 
  * @param data, type=array, receive data from server 
  * plot the data dynamically based on data from database 
-*/ 
+ */ 
 const graphing =  function(data) {
   const workload = []
   const staffCost = []
@@ -161,8 +228,6 @@ const graphing =  function(data) {
     workload.push({"label":data[i][0], "y":data[i][1]})
     staffCost.push({"label":data[i][0], "y":data[i][2]})
   };
-
-
   var stack = document.getElementById("stack")
   var stack_chart = new CanvasJS.Chart(stack, {
 
@@ -171,36 +236,202 @@ const graphing =  function(data) {
   title:{
     text: "Workload VS Total Cost"              
   },
+  data: [    
+    {     
+      type: "column",
+      axisYIndex: 0,
+      name:"StaffCost",
+      showInLegend: true,
+      dataPoints: staffCost
+    },
+    { //dataSeries - second quarter
 
-  data: [  //array of dataSeries     
-  {     
-    type: "column",
-    name: "Workload",
-    showInLegend: true,
-    dataPoints: workload
-  },
-
-  { //dataSeries - second quarter
-
-  type: "column",
-  name: "StaffCost", 
-  showInLegend: true,               
-  dataPoints: staffCost
-}
-],
+      type: "column",
+      axisYIndex: 0,
+      axisYType: "secondary",
+      name:"Workload", 
+      showInLegend: true,               
+      dataPoints: workload
+    }
+  ],
 /** Set axisY properties here*/
-  axisY:{
-    prefix: "$",
-  }    
-});
+    axisY:[
+      {
+        title: "StaffCost",
+        minimum:0,
+        prefix: "$",
+      }
+    ],
+    axisY2:[{
+      suffix:"Hour",
+      minimum:0,
+      title: "Workload",
+    }]   
+  });
 
-stack_chart.render()
+  stack_chart.render()
 }
+
+// function to check whether file has a specific extension 
+function checkFile(send) {
+  const extension = new Array('.xlsx','.xls'); 
+  var fileExtension = send.value;
+  // get the file extension 
+  fileExtension = fileExtension.substring(fileExtension.lastIndexOf('.'));
+  if (!extension.includes(fileExtension)) {
+    alert("Invalid file selected, valid files are of " +extension.toString() + " types.")
+    send.value='';
+    return false;
+  }
+  else {
+    return true
+  }
+
+}
+
+// click to show user information 
+function sampleInformation() {
+  alert('Excel files includes files that have extension .xlsx and .xls. \
+  For example: "file1.xlsx" or "file1.xls"')
+}
+
+
+/**
+ * function that take an event when user click 
+ * used the jquery function toggle() to display and hide 
+ *
+ * @param click event  
+*/ 
+$('#comment').on('click', function(event) {
+  event.preventDefault()
+  $('#table_comments').toggle();
+  $('#unitcodes').toggle();
+  $('#unitcodelabels').toggle();
+  $('#years').toggle();
+  $('#yearlabels').toggle();
+  $('.clear').toggle();
+  $('#semester').toggle();
+  $('#semesterlabel').toggle();
+})
+
+/**
+ * function take an array of data  
+ * array data that pass in the create table to create the table dynamically 
+ *
+ * @param an array of the data 
+*/ 
+function create_table(data) {
+  const name = document.getElementById('table_comments')
+  for(var i = 0; i < data.length; i++)
+  {
+    var newRow = name.insertRow(name.length);
+    for(var j = 0; j < data[i].length; j++) {
+      var cell = newRow.insertCell(j);
+      cell.innerHTML = data[i][j]
+    }
+  }
+}  
+
+/**
+ * function fetch the data with comment and return a response of an array of data 
+ * pass into the create table to create the table 
+ *
+ * @param year which is the input user what year they want .
+ * @param semester which is the input user what semester what they want 
+ * @param unitcode which is the input user what unitcode what they want.
+*/ 
+var sendComment = async(year,semester, unitcode) => {
+  const requestParams = buildCommentParams(year, semester, unitcode);
+  const result = await fetch('http://127.0.0.1:5000/comment?' + requestParams , {
+    method:"GET",
+    headers:{
+      headers: {
+        "Content-Type":"application/json"
+      }
+    }
+  })
+  const data = await result.json()
+  create_table(data)
+  return data;
+}
+
+
+/**
+ * function take when button is clicked, 
+ * it will remove the table
+ * and it will remove unitcodes, years and semester input 
+ *
+ * @param take no parameter 
+*/ 
+function clearFunction() {
+  $("#table_comments tr>td").remove();
+  document.getElementById('unitcodes').value='';
+  document.getElementById('years').value='';
+  document.getElementById('semester').value='';
+}
+
+
+/**
+ * function that check when user enter in the input, 
+ * and it will take the value and pass into the sendComment function
+ * and it will remove unitcodes, years and semester input 
+ *
+ * @param take no parameter 
+*/ 
+const updateComment = async() => {
+  const yearValue = $("#years").val().toString();
+  const semesterValue = $("#semester").val().toString();
+  const unitValue = $("#unitcodes").val().toString();
+
+  sendComment(yearValue,semesterValue,unitValue)
+}
+
+/**
+ * function that handle click event by 
+ * detect the enter key  
+ *
+ * @param click event  
+*/ 
+const handleClick = (e)=>{
+  if(e.keyCode === 13){
+    updateComment()
+  }
+}
+
+/**
+ * function that handle click event by 
+ * detect the delete key  
+ *
+ * @param take no parameter 
+*/ 
+const unit= document.getElementById('unitcodes');
+const year = document.getElementById('years')
+const semester = document.getElementById('semester')
+unit.onkeydown = function() {
+    var key = event.keyCode || event.charCode
+    if( key == 8 || key == 46 )
+      $("#table_comments tr>td").remove();
+};
+year.onkeydown = function() {
+  var key = event.keyCode || event.charCode
+  if( key == 8 || key == 46 )
+    $("#table_comments tr>td").remove();
+};
+semester.onkeydown = function() {
+  var key = event.keyCode || event.charCode
+  if( key == 8 || key == 46 )
+    $("#table_comments tr>td").remove();
+};
+
+
 
 getAllData()
 getWorkLoadData()
 getEmployeeData()
 graphing()
+
+
+
 
 
 
